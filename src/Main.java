6ee1f4e7e4ccc941779c83bc221eb1e2268c4e35
kymdennis3 <kymdennis3@gmail.com>
2022-11-19 16:28:42 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // nested loops = a loop inside a loop
+
+       Scanner scanner = new Scanner(System.in);
+
+       int rows;
+       int columns;
+       String symbol;
+
+        System.out.println("Enter number of rows:");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter number of columns:");
+        columns = scanner.nextInt();
+
+        System.out.println("Enter a symbol:");
+        symbol = scanner.next();
+
+       for(int i = 1; i<=rows; i++){
+           System.out.println();
+           for(int j = 1; j<=columns; j++){
+               System.out.print(symbol);
+           }
+
+       }
     }
 }
